4e80a52e1ea10926ecc245218e12025e7afb1ccd
Monace1 <morganchuma654@gmail.com>
2024-07-03 09:58:51 +0300

    first commit

---
+++ Session-service-in-jwt/src/main/java/com/Emtech/Session/service/in/jwt/SessionServiceInJwtApplication.java
@@ -0,0 +1,13 @@
+package com.Emtech.Session.service.in.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionServiceInJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionServiceInJwtApplication.class, args);
+	}
+
+}
---
+++ Session-service-in-jwt/src/test/java/com/Emtech/Session/service/in/jwt/SessionServiceInJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Emtech.Session.service.in.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionServiceInJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
